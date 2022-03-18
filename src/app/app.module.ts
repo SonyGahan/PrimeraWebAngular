4a93125d3c416525306e8e700c2c33601588b46e
SonyGahan <94231488+SonyGahan@users.noreply.github.com>
2022-03-18 17:57:10 -0300

    diseño con posiciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +10,10 @@ import { EstInformaticosComponent } from './componentes/est-informaticos/est-inf
 import { ExpLaboralComponent } from './componentes/exp-laboral/exp-laboral.component';
 import { ProyFreelanceComponent } from './componentes/proy-freelance/proy-freelance.component';
 import { CompetenciasComponent } from './componentes/competencias/competencias.component';
+import { HttpClientModule} from '@angular/common/http';
+import { AcercadeComponent } from './componentes/acercade/acercade.component';
+import { LoginComponent } from './componentes/login/login.component';
+
 
 @NgModule({
   declarations: [
@@ -20,11 +24,14 @@ import { CompetenciasComponent } from './componentes/competencias/competencias.c
     EstInformaticosComponent,
     ExpLaboralComponent,
     ProyFreelanceComponent,
-    CompetenciasComponent
+    CompetenciasComponent,
+    AcercadeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
